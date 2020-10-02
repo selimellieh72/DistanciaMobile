e9ee07c2b70fe3cf4cb5a4792d0e597ce5ff6f93
Posenega <68822544+Posenega@users.noreply.github.com>
2020-10-02 16:46:16 +0300

    add homework detail

--- lib/widgets/homeworks/homework_item.dart
+++ lib/widgets/homeworks/homework_item.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeworkItem extends StatefulWidget {
   HomeworkItem({
@@ -37,90 +40,151 @@ class _HomeworkItemState extends State<HomeworkItem> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GestureDetector(
-        onTap: () {
-          _toggleIsExpanded();
-          print(_isExpanded);
-        },
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(33),
-                color: Color.fromRGBO(112, 112, 112, 1),
-              ),
-              height: 80,
-              child: Stack(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        flex: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.all(1),
-                          child: FittedBox(
+        padding: const EdgeInsets.all(8.0),
+        child: GestureDetector(
+            onTap: () {
+              _toggleIsExpanded();
+              print(_isExpanded);
+            },
+            child: Column(children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(33),
+                  color: Color.fromRGBO(112, 112, 112, 1),
+                ),
+                height: 65,
+                child: Stack(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.all(1),
                             child: FittedBox(
-                              child: Text(
-                                widget.title,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                              child: FittedBox(
+                                child: Text(
+                                  widget.title,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Flexible(
-                        flex: 7,
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          alignment: Alignment.center,
-                          child: Text(
-                            _formatInstructions(widget.instructions),
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
+                        Flexible(
+                          flex: 7,
+                          child: Container(
+                            padding: EdgeInsets.all(0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              _formatInstructions(widget.instructions),
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Flexible(
-                        fit: FlexFit.tight,
-                        flex: 2,
-                        child: Text(
-                          DateFormat('E MMM\nh:m').format(widget.date),
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
+                        Flexible(
+                          fit: FlexFit.tight,
+                          flex: 2,
+                          child: Text(
+                            DateFormat('E MMM\nh:m').format(widget.date),
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  width: _isExpanded ? 340 : 0,
+                  height: _isExpanded ? 200 : 0,
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(33),
+                        bottomRight: Radius.circular(33)),
+                    color: Color.fromRGBO(42, 42, 42, 1),
                   ),
-                ],
-              ),
-            ),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              height: _isExpanded ? 150 : 0,
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(33),
-                color: Color.fromRGBO(42, 42, 42, 1),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                  child: _isExpanded
+                      ? SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Text(
+                                  widget.title,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: Colors.white),
+                                      width: 150,
+                                      height: 100,
+                                      child: SingleChildScrollView(
+                                        child: Container(
+                                          margin: EdgeInsets.all(5),
+                                          child: Text(
+                                            widget.instructions,
+                                            style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    42, 42, 42, 1),
+                                                fontSize: 17),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Icon(
+                                        MdiIcons.clockAlert,
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      : Align(
+                          alignment: Alignment.bottomCenter,
+                          child: _isExpanded
+                              ? Icon(
+                                  Icons.arrow_drop_up,
+                                  color: Colors.white,
+                                  size: 30.0,
+                                )
+                              : Icon(
+                                  Icons.arrow_drop_down,
+                                  color: Colors.white,
+                                  size: 30.0,
+                                ),
+                        )),
+            ])));
   }
 }
