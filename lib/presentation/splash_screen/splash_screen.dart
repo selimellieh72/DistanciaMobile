72f0c26c208c90545e1d3229efe39f9f2ff77975
Posenega <68822544+Posenega@users.noreply.github.com>
2020-10-13 22:20:41 +0300

    iscons

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -5,8 +5,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:edulb/application/auth/auth_bloc.dart';
 import 'package:edulb/presentation/auth/auth_screen.dart';
 import 'package:edulb/presentation/homeWidget/home_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatelessWidget {
+  static const routeName = '/splash-screen';
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthBloc, AuthState>(
@@ -20,8 +22,32 @@ class SplashScreen extends StatelessWidget {
         );
       },
       child: Scaffold(
-        body: Center(
-          child: Text('Loading...'),
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            Positioned(
+              top: 175,
+              right: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 210,
+                    width: 210,
+                    child: Image.asset('assets/IMAGE/distancia.png'),
+                  ),
+                  Text(
+                    'Distancia',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color.fromRGBO(42, 42, 42, 1),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
